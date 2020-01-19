dda5300348836f5a4dc61af5c2fb2e6175976921
BenWhiteside <meisben@hotmail.com>
2020-01-19 23:27:32 +1100

    Cleaned up code after figuring out that the internet was wrong, but Nielsen and Chuang were correct.
    Signed-off-by: BenWhiteside <meisben@hotmail.com>

--- testSuperdenseCoding.py
+++ testSuperdenseCoding.py
@@ -19,11 +19,12 @@ def show_figure(fig):
 
 
 # Use Aer
-simulator = Aer.get_backend('qasm_simulator')
+backend = Aer.get_backend('qasm_simulator')
+print('Provider: ', backend)
 
 # Create two registers: one quantum, one classical
-q = qr(2)
-c = cr(2)
+q = qr(2, 'q')
+c = cr(2, 'c')
 circuit = qc(q, c)
 
 # Add a Hadamard gate on q0 to create superposition
@@ -35,12 +36,13 @@ circuit.cx(q[0], q[1])
 
 # Add a X gate to q0 to encode message:
 # | Message | Pauli Gate | Measurement
-# | 00      | I          | 00 (100%)
-# | 01      | X          | 10 (100%)
-# | 10      | Z          | 01 (100%)
+# | 00      | iden       | 00 (100%)
+# | 01      | Z          | 01 (100%)
+# | 10      | X          | 10 (100%)
 # | 11      | XZ         | 11 (100%)
-circuit.x(q[0])
 circuit.z(q[0])
+# circuit.x(q[0])
+# circuit.iden(q[0])
 
 # Add another CNOT gate as before to entangle
 # the encoded message and q1:
@@ -51,6 +53,7 @@ circuit.cx(q[0], q[1])
 circuit.h(q[0])
 
 # Add a measurement gate to obtain the measurement
+circuit.barrier()
 circuit.measure(q, c)
 
 # Plot Circuit
@@ -59,7 +62,7 @@ show_figure(diagram)
 
 # Execute the circuit on the QASM Simulator
 start_time = datetime.now()
-job = execute(circuit, simulator, shots=100000)
+job = execute(circuit, backend, shots=100000)
 end_time = datetime.now()
 print('Duration: {}'.format(end_time - start_time))
 
@@ -71,4 +74,3 @@ counts = result.get_counts(circuit)
 print('\nTotal count for 00 and 11 are:', counts)
 plot_histogram(counts);
 show_figure(plot_histogram(result.get_counts(circuit)))
-
