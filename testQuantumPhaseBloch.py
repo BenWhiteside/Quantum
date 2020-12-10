1bfcb80aeef9a7859de5e1abde705bb68b745b17
BenWhiteside <meisben@hotmail.com>
2020-12-11 09:40:13 +1100

    Updated definition of show_figure as the show function no longer needs to be passed a figure.

--- testQuantumPhaseBloch.py
+++ testQuantumPhaseBloch.py
@@ -9,7 +9,7 @@ def show_figure(fig):
     new_mngr = new_fig.canvas.manager
     new_mngr.canvas.figure = fig
     fig.set_canvas(new_mngr.canvas)
-    plt.show(fig)
+    plt.show()
 
 # Define the Quantum Register
 q = QuantumRegister(1)
