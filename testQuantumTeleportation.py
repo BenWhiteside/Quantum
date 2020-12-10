6f66f34902665b8aa9c2dee8f82cf2e930930f70
BenWhiteside <meisben@hotmail.com>
2020-12-11 09:40:29 +1100

    Updated definition of show_figure as the show function no longer needs to be passed a figure.

--- testQuantumTeleportation.py
+++ testQuantumTeleportation.py
@@ -17,7 +17,7 @@ def show_figure(fig):
     new_mngr = new_fig.canvas.manager
     new_mngr.canvas.figure = fig
     fig.set_canvas(new_mngr.canvas)
-    plt.show(fig)
+    plt.show()
 
 
 ''' Transform the computational basis in to the Bell basis'''
